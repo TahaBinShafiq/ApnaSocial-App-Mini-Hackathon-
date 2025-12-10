9b108001a717aa59e66f9d4928f673ce97caa5e5
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-12-10 20:31:46 +0500

    add tostify

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -71,7 +71,11 @@ function createPost() {
     document.getElementById("post-input").value = "";
     document.getElementById("post-url").value = "";
 
-    alert("Post saved successfully!");
+    Toastify({
+      text: "Post saved successfully!",
+
+      duration: 3000,
+    }).showToast();
   };
 
   if (postImageFile) {
@@ -95,8 +99,11 @@ function deletePost(index) {
   allPosts.splice(index, 1);
 
   localStorage.setItem("posts", JSON.stringify(allPosts));
-
   renderPosts();
+  Toastify({
+    text: "Post delete successfully!",
+    duration: 3000,
+  }).showToast();
 }
 
 function toggleLike(index) {
@@ -231,6 +238,10 @@ function saveEditedPost() {
     localStorage.setItem("posts", JSON.stringify(allPosts));
     closeEditModal();
     renderPosts();
+     Toastify({
+    text: "Post edit successfully!",
+    duration: 3000,
+  }).showToast();
   };
 
   if (newImage) {
@@ -252,7 +263,7 @@ function filterPosts(query) {
 
   const allPosts = JSON.parse(localStorage.getItem("posts")) || [];
 
-  const filteredPosts = allPosts.filter(post => 
+  const filteredPosts = allPosts.filter((post) =>
     post.content.toLowerCase().includes(query.toLowerCase())
   );
 
@@ -267,35 +278,41 @@ function filterPosts(query) {
     feedContainer.innerHTML += `
       <div class="bg-gray-800 p-4 sm:p-6 rounded-xl shadow-2xl mb-6 border border-gray-700 hover:border-emerald-600/50">
         <div class="flex items-center mb-4">
-          <div class="w-10 h-10 rounded-full bg-emerald-600 text-white flex items-center justify-center font-bold">${post.ownerName.charAt(0)}</div>
+          <div class="w-10 h-10 rounded-full bg-emerald-600 text-white flex items-center justify-center font-bold">${post.ownerName.charAt(
+            0
+          )}</div>
           <div class="ml-3">
             <p class="font-semibold text-gray-100">${post.ownerName}</p>
             <p class="text-xs text-gray-400">${post.time}</p>
           </div>
         </div>
         <p class="text-gray-300 mb-4">${post.content}</p>
-        ${post.postUrl ? `<img class="w-full h-auto max-h-80 object-cover rounded-lg mb-4 border border-gray-700" src="${post.postUrl}" alt="Post image" />` : ""}
+        ${
+          post.postUrl
+            ? `<img class="w-full h-auto max-h-80 object-cover rounded-lg mb-4 border border-gray-700" src="${post.postUrl}" alt="Post image" />`
+            : ""
+        }
         <div class="flex justify-around">
           <button onclick="toggleLike(${index})" class="flex items-center text-emerald-400 hover:text-emerald-300 transition-all transform hover:-translate-y-0.5">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
         </svg>${post.likes}
           </button>
-          ${isOwner 
-            ? `<button onclick="openEditModal(${index})" class="flex items-center text-blue-400 hover:text-blue-300 transition-all transform hover:-translate-y-0.5"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          ${
+            isOwner
+              ? `<button onclick="openEditModal(${index})" class="flex items-center text-blue-400 hover:text-blue-300 transition-all transform hover:-translate-y-0.5"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
           </svg> Edit</button>
                <button onclick="deletePost(${index})" class="flex items-center text-red-500 hover:text-red-400 transition-all transform hover:-translate-y-0.5"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg> Delete</button>`
-            : ""
+              : ""
           }
         </div>
       </div>
     `;
   });
 }
-
 
 const searchInput = document.getElementById("search-posts");
 searchInput.addEventListener("input", (e) => {
